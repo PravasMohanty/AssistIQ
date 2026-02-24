99acdd85fbf930d0a2c212095c95056c9c0646f0
Pravas <pravasmohanty1000@gmail.com>
2026-02-24 20:00:31 +0530

    Setting up LLM Functionalities

--- server/routes/kbRouter.js
+++ server/routes/kbRouter.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const router = express.Router()
+const KBRouter = express.Router()
 const { addKbContent, searchKbContent, resolveKbQuery } = require('../controllers/kbController')
 const auth = require('../middleware/auth')
 
-// Protected route to add knowledge base entries
-router.post('/add', auth, addKbContent)
+KBRouter.post('/add', auth, addKbContent)
 
-// Public routes for searching and resolving queries (can be protected if needed)
-router.post('/search', searchKbContent)
-router.post('/resolve', resolveKbQuery)
+KBRouter.post('/search', searchKbContent)
+KBRouter.post('/resolve', resolveKbQuery)
 
-module.exports = router
+module.exports = KBRouter
